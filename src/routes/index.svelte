88f0c624f586f08f1a47f8ee8b78863d294d2312
hippokrates1337 <j.weber_2003@web.de>
2022-04-16 11:02:54 +0000

    Add query functionality

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,19 @@
-<script context="module" lang="ts">
+<script>
 	export const prerender = true;
+
+	import AddIngredient from "$lib/AddIngredient.svelte"
+	import IngredientQuery from "$lib/IngredientQuery.svelte"
+
+	let ingredients = []
+
+	const addIngredient = (ingredient) => {
+		ingredients = [...ingredients, ingredient]
+	}
 </script>
 
 <svelte:head>
 	<title>Home</title>
 </svelte:head>
 
+<AddIngredient on:add={e => addIngredient(e.detail)} />
+<IngredientQuery {ingredients} />
