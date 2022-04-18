69127f22301d7c66e93078405b90ee91c072d50b
hippokrates1337 <j.weber_2003@web.de>
2022-04-18 05:42:57 +0000

    Add route for popularity-based recommendations

--- src/routes/recommendations/_api.js
+++ src/routes/recommendations/_api.js
@@ -1,4 +1,4 @@
-import {retrievePopularIngredients} from "$lib/database"
+import {retrievePopularIngredients, retrievePopularRecommendations} from "$lib/database"
 
 export const api = async (request, data) => {
     let status = 500
@@ -6,7 +6,13 @@ export const api = async (request, data) => {
 
     switch(request.method.toUpperCase()) {
         case "GET":
-            let response = await retrievePopularIngredients()
+            let response
+            if(data) {
+                response = await retrievePopularRecommendations(data)
+            } else {
+                response = await retrievePopularIngredients()
+            }
+            
             body = response.body
             status = response.status;
             break;
