b1819ebbbeebed88caadea660a787882c740e263
hippokrates1337 <j.weber_2003@web.de>
2022-04-23 14:19:32 +0000

    Made recommended ingredients clickable

--- src/lib/IngredientQuery.svelte
+++ src/lib/IngredientQuery.svelte
@@ -9,29 +9,25 @@
     }
 </script>
 
-<div class="h6 text-primary mt-3">
-    <label class="mb-2" for="list_ingredientquery" id="label_ingredientquery">
+<div class="mt-3">
+    <label class="h6 text-primary mb-2" for="list_ingredientquery" id="label_ingredientquery">
         Ingredients currently in your query:
     </label>
     {#each ingredients as ingredient}
         <span class="badge bg-secondary m-1">
-            {ingredient}
-            <button class="btn btn-sm shadow-none" on:click={() => onRemove(ingredient)}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" class="bi bi-trash" viewBox="0 0 16 16">
-                    <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
-                    <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
-                </svg>
-            </button>
+            <div class="row">
+                <div class="col my-auto">
+                    {ingredient}
+                </div>
+                <div class="col text-end">
+                    <button class="btn btn-sm shadow-none" on:click={() => onRemove(ingredient)}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" class="bi bi-trash" viewBox="0 0 16 16">
+                            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+                            <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+                        </svg>
+                    </button>
+                </div>
+            </div>
         </span>
     {/each}
 </div>
-
-<!--
-<ul role="list" class="list-group list-group-flush" id="list_ingredientquery" aria-labelledby="label_ingredientquery">
-        {#each ingredients as ingredient}
-            <li class="list-group-item list-group-item-action list-group-item-secondary">
-                {ingredient}
-            </li>
-        {/each}
-    </ul>
--->
