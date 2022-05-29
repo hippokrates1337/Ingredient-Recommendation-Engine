f3175db899c03a2525770873c2ee16ad2bfa85cb
hippokrates1337 <j.weber_2003@web.de>
2022-05-29 20:33:57 +0000

    Close #27

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -4,6 +4,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://use.fontawesome.com/59baeab75a.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
 </svelte:head>
 
 <slot />
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -89,10 +89,7 @@
 			</strong>
 			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarHeader"
 			aria-controls="navbarHeader" aria-expanded="true" aria-label="expand navigation">
-				<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-in-up text-white" viewBox="0 0 16 16">
-					<path fill-rule="evenodd" d="M3.5 10a.5.5 0 0 1-.5-.5v-8a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 .5.5v8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 0 0 1h2A1.5 1.5 0 0 0 14 9.5v-8A1.5 1.5 0 0 0 12.5 0h-9A1.5 1.5 0 0 0 2 1.5v8A1.5 1.5 0 0 0 3.5 11h2a.5.5 0 0 0 0-1h-2z"/>
-					<path fill-rule="evenodd" d="M7.646 4.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V14.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3z"/>
-			  	</svg>
+				<i class="bi bi-info-circle" />
 			</button>
 		</div>
 	</div>
@@ -102,7 +99,7 @@
 	<section class="py-5 text-center container">
 		<div class="row py-lg-5">
 			<div class="col-lg-6 col-md-8 mx-auto">
-				<AddIngredient {allowedIngredients} query_ingredients={ingredients} on:add={e => addIngredient(e.detail)} />
+				<AddIngredient {allowedIngredients} queryIngredients={ingredients} on:add={e => addIngredient(e.detail)} />
 			</div>
 		</div>
 	</section>
